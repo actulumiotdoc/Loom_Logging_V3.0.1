37c8046efabeebd1396b3a9a6d57eca1411421c2
actulumiotdoc <actulumiotdoc@gmail.com>
2026-06-23 13:59:58 +0700

    Enhance installation script for LoomLogging
    Updated the script to create necessary directories and databases, including telemetry tables. Improved checks for existing directories and databases, and added configuration file creation.

--- update_and_install_LoomLogging_V3.0.1.sh
+++ update_and_install_LoomLogging_V3.0.1.sh
@@ -1,21 +1,71 @@
 #!/bin/bash
 user=$HOME
-stdDir=sqlite/db
-db=sqlite/db/factory.db
+sqldir=telemetry/sql/
+pydir=telemetry/py/
+db=$user/telemetry/sql/telemetry_factory.db
+conf=$user/telemetry/conf.json
+nodered=$user/.node-red/flows.json
 
 sudo apt update && sudo apt -y upgrade
-if [ ! -d "$stdDIr" ]; then
-  mkdir -p $stdDIr
+if [ ! -d "$sqldir" ]; then
+  mkdir -p $sqldir
   echo "Create Sqlite Directory..."
-else
-  echo "Already has Sqlite Directory..."
-fi
+  if [ ! -f "$db" ]; then
+    sqlite3 "$db" "
+    CREATE TABLE telemetry_local (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      create_at DATETIME DEFAULT (datetime('now', '+7 hours')),
+      sent INTEGER DEFAULT 0,
+      ts INTEGER NOT NULL,
+      total_meter REAL,
+      total_a REAL,
+      total_b REAL,
+      nta_meter REAL,
+      ntb_meter REAL,
+      ota_meter REAL,
+      otb_meter REAL,
+      wking_a REAL,
+      wking_b REAL,
+      speed_main REAL,
+      speed_take REAL
+      );"
 
-if [ ! -f "$db" ]; then
   sqlite3 "$db" "
-  CREATE TABLE raw_data_sensors (
+  CREATE TABLE telemetry_meter (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     create_at DATETIME DEFAULT (datetime('now', '+7 hours')),
+    m0 REAL,
+    m1 REAL,
+    m2 REAL,
+    m3 REAL,
+    m4 REAL,
+    m5 REAL,
+    m6 REAL,
+    m7 REAL,
+    m8 REAL,
+    m9 REAL,
+    m10 REAL,
+    m11 REAL,
+    m12 REAL,
+    m13 REAL,
+    m14 REAL,
+    m15 REAL,
+    m16 REAL,
+    m17 REAL,
+    m18 REAL,
+    m19 REAL,
+    m20 REAL,
+    m21 REAL,
+    m22 REAL,
+    m23 REAL
+  );"
+
+  sqlite3 "$db" "
+  CREATE TABLE telemetry_could (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    create_at DATETIME DEFAULT (datetime('now', '+7 hours')),
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sent INTEGER DEFAULT 0,
     ts INTEGER NOT NULL,
     total_meter REAL,
     total_a REAL,
@@ -24,15 +74,39 @@ if [ ! -f "$db" ]; then
     ntb_meter REAL,
     ota_meter REAL,
     otb_meter REAL,
+    work_a INTEGER,
+    work_b INTEGER,
     hour_meter REAL,
     speed_main REAL,
-    speed_take REAL,
-    sent INTEGER DEFAULT 0,
-    sent_at INTEGER,
-  );
-  "
-  echo "Create DataBase Succesfully..."
+    speed_take REAL
+  );"
+    echo "Create DataBase Succesfully..."
+  else
+    echo "Already has Database..."
+  fi
 else
-
-  echo "Already has Database..."
+  echo "Already has Sqlite Directory..."
 fi
+
+if [ ! -d "$pydir" ]; then
+  mkdir -p $pydir 
+  echo "Create Python Directory..."
+else
+  echo "Already has Python Directory..."
+fi
+
+if [ ! -f "$conf"]; then
+  touch "$conf"
+  code=$(cat $HOME/loom/influxdb/device.txt)
+  cat << 'EOJ' >> "$conf"
+    "code": "$code",
+    "circumferance": "-",
+    "gear-ratio": "-",
+  'EOJ'
+fi
+
+#node-red-flow
+rm -rf "$nodered"
+cat << 'EON' > "$nodered"
+  \
+  EON
